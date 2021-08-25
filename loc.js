df598050d4b985c886c05b66b3fc2ef2601846a0
tejasisboss <65665875+tejasisboss@users.noreply.github.com>
2021-08-25 14:11:52 +0530

    Add files via upload

---
+++ loc.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "latitude = "+position.coords.latitude;
+  x.innerHTML += "<br />"
+  x.innerHTML += "longitude = "+position.coords.longitude
+}
